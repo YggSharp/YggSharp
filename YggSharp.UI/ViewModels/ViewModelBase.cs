8436200ed27c83a690d55b5eb65ca5ab584de6fa
WGOS <thewgosplayer@gmail.com>
2023-01-08 12:26:28 +0400

    UI init

---
+++ YggSharp.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace YggSharp.UI.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ YggSharp.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace YggSharp.UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if(!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            return;
+        
+        BeginMoveDrag(e);
+    }
+}
